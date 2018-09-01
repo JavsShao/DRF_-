43fa504dacfc60aab2d1a6bb27144a49cc99a930
JavsShao <javs_shao@163.com>
2018-09-01 17:45:41 +0800

    清理代码冗余

--- library/booktests/serializers.py
+++ library/booktests/serializers.py
@@ -7,19 +7,10 @@ from rest_framework.validators import UniqueValidator,UniqueTogetherValidator
 class BookSerializer(serializers.ModelSerializer):
 
     class Meta:
-        #元选项，就可以理解为 进行一些配置
         model = BookInfo        #必须要设置关联的模型
-        #fields = '__all__'      #必须设置fields  __all__表示所有字段
-        #制定字段
-        # fields = ['id','readcount','name','commentcount']
-
         #设置只读字段
         read_only_fields = ['id','name']
-
         exclude = ['image']
-
-
-
         #额外设置 字段的选项
         extra_kwargs = {
             # “字段名”：{"选项名"：选项值}  语法
@@ -32,31 +23,9 @@ class BookSerializer(serializers.ModelSerializer):
 
         }
 
-
-# 联合主键
-# 联合唯一键
-
-# name 字段 和 pub_date 字段为 联合唯一键
-
-# name-pub_date
-
-# 射雕英雄传-2000-1-1    这是一条唯一的记录
-
-# 射雕英雄传-2000-1-2    这是一条唯一的记录
-
-# 射雕英雄前传-2000-1-1    这是一条唯一的记录
-
-# 射雕英雄传-2000-1-1    就冲突了！@！！！！！！
-
-
-#  Django文档
-# 串行器字段处理原始值和内部数据类型之间的转换。
-    # 模型 --》JSON    序列化
-    # JSON --》模型    反序列化
 # 他们还处理验证输入值
 class BookInfoSerializer(serializers.Serializer):
 
-    #required	表明该字段在反序列化时必须输入，默认True
 
     def custom_validate(value):
         raise serializers.ValidationError('我就是来捣乱的')
@@ -90,45 +59,8 @@ class BookInfoSerializer(serializers.Serializer):
 
     #实现 create 方法
     def create(self, validated_data):
-        #validated_data
-        # 验证后的数据，已经通过我们的层层验证
-        # {‘name’:'xxxx',...}
 
-        #BookInfo(**validated_data) 这样数据不能直接入库
-        # book =BookInfo(**validated_data)
-        # book.save()
-        # return book
-
-        #也可以这样操作
         return BookInfo.objects.create(**validated_data)
-
-    #把name设置为唯一的
-    # name = serializers.CharField(max_length=20,validators=[
-    #     UniqueValidator(queryset=BookInfo.objects.all())
-    # ])
-
-    #多个字段校验
-    # def validate(self, data):
-    # def validate(self, attrs):
-    #     # attrs 就是我们传递过来的data
-    #
-    #     readcount = attrs.get('readcount')
-    #     commentcount = attrs['commentcount']
-    #     if readcount<commentcount:
-    #         raise serializers.ValidationError('评论量不能大于阅读量')
-    #
-    #     #如果数据没有问题就把数据返回
-    #     return attrs
-    #
-    #
-    #
-    # # validate_字段名（self,value）
-    # def validate_readcount(self,value):
-    #
-    #     if value<0:
-    #         raise serializers.ValidationError('阅读量不能为负数')
-    #
-    #     return value
 
 
 
--- library/booktests/urls.py
+++ library/booktests/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns =[
-    #url(r'^books/$',views.BookGenericAPIView.as_view(),name='view_name'),
+    url(r'^books/$',views.BookGenericAPIView.as_view(),name='view_name'),
     url(r'^books/(?P<pk>\d+)/$',views.BookGenericAPIView.as_view(),name='view_name'),
 ]
 
