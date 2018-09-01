d6e10bc1feaae646041c4c1340c08df406884aa4
JavsShao <javs_shao@163.com>
2018-09-01 15:33:53 +0800

    创建序列化器_图书数据序列化器

--- library/booktests/serializers.py
+++ library/booktests/serializers.py
@@ -5,6 +5,8 @@ from library.booktests.models import BookInfo
 
 class BookInfoSerializer(serializers.ModelSerializer):
     """图书数据序列化器"""
-    class Meta:
-        model = BookInfo
-        fields = '__all__'
+    id = serializers.IntegerField(label='ID',read_only=True)
+    btitle = serializers.CharField(label='名称',max_length=20)
+    bpub_data = serializers.DateField(label='发布日期',required=False)
+    bcomment = serializers.IntegerField(label='评论量',required=True)
+    image = serializers.ImageField(label='图片',required=False)
