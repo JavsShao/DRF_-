d4324f5252cfccd4a8607516c943dcfa61459200
JavsShao <javs_shao@163.com>
2018-09-01 15:27:42 +0800

    安装MySQL数据库驱动程序  __init__.py

--- library/booktests/views.py
+++ library/booktests/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .serializers import BookInfoSerializer
+from .models import BookInfo
+
+
